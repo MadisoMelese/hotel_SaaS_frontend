83b6363082c90a848f1f568ccc1edd446f00d347
Madiso Melese <madishamadiso00@gmail.com>
2026-04-03 16:25:57 +0300

    services/dashboard.service.js hotel dashboard modified

--- frontend/src/services/dashboard.service.js
+++ frontend/src/services/dashboard.service.js
@@ -2,7 +2,7 @@ import api from './api'
 
 export const dashboardService = {
   getStats: async () => {
-    const response = await api.get('/dashboard')
+    const response = await api.get('/dashboard/stats')
     return response.data.data
   },
 
